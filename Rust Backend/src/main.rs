5b1f7c9b6bcb0edabb21d70f117ea0e99d9c7cce
= <=>
2024-09-29 21:06:21 +0530

    sign in

--- Rust Backend/src/main.rs
+++ Rust Backend/src/main.rs
@@ -1,9 +1,20 @@
+use actix_cors::Cors;
+use actix_web::http::header;
 use actix_web::{get, post, App, HttpResponse, HttpServer};
+use Todo_Rust::signin;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new())
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:5173/signin")
+            // .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+            // .allowed_headers(vec![header::CONTENT_TYPE, header::AUTHORIZATION])
+            // .supports_credentials()
+            .max_age(3600);
+        App::new().wrap(cors).service(signin::signin_user)
+    })
+    .bind(("127.0.0.1", 3000))?
+    .run()
+    .await
 }
